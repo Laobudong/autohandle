c2a4c5046f62f55c568f6e5ab7f98c9b67738a90
laobudong <zh_bai@163.com>
2018-09-05 14:09:57 +0800

    git add *.txt

---
+++ list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+list_zi = ['增','值','税']
+print ("list_zi",list_zi,'lenth',len(list_zi))
+print ("list_zi(0)",list_zi[0])
+print ("list_zi(2)",list_zi[2],'lenth=',len(list_zi[2]))
+list_zi.append('类')
+print ("list_zi",list_zi,'lenth',len(list_zi))
+list_ci = '一般纳税人'
+print ("list_ci",list_ci,'lenth',len(list_ci))
+
+print ("list_zi(0)",list_ci[0])
+print ("list_zi(2)",list_ci[2])
+''' list  用[] append增加 pop(i)删除 insert(i,'Jack')
+tuple 用()
+多行注释
+'''
---
+++ py11.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print("test")
+print("汉字！")
+help()
