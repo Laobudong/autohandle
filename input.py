192cb60d3256cadf4c0265680f4baebd74a1f7dd
laobudong <zh_bai@163.com>
2018-09-06 09:57:05 +0800

    git commit

--- input.py
+++ input.py
@@ -1,10 +1,14 @@
+# raw_input
 def get_input( prompt ):
-    value = input( prompt )
+    value = raw_input( prompt )
     for letter in value:
-        if letter < 'a' or letter > 'z':
+        if letter < "a" or letter > "z":
             print( "The character", letter, "is not allowed!")
             #value = get_input( prompt ) # DO NOT DO THIS!
     return value
 
 s = get_input( "Give a string of lower case letters: " )
 print( "The user entered:", s )
+
+d = input( "Give a nember: " )
+print( "The user entered:", d )
--- py11.py
+++ py11.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 
 print("test")
 print("汉字！")
-help()
+#help()
+print ('OS-dir',os.curdir)
+print('OSname',os.name)
